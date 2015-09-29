59861c64b192f613f109c12417d27e787ccfd01f
Duncan Marsh <dwolfm@gmail.com>
2015-09-29 15:04:01 -0700

    added git ignore to ignore fake home dir

--- src/bash-starter.sh
+++ src/bash-starter.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
-echo $1
+# check os
+# backup old bashconf
+# install bash git aware prompt
+# set PS1
+# set convienece alisus
+# link bashrc bash_profile and profile to be the same
+
+echo "OS input $1"
+echo ""
+
+HOME="./USER_TEST"
 
 # check os
 OS=$1
@@ -10,13 +20,17 @@ if [ $OS = "Linux" ]; then
 elif [ $OS = "Darwin" ]; then
   LSColorFlag="-G"
 else
-  echo "This script must be ran on Linux or OS X"
+  echo "This script must be run on a Linux or OS X Operating System."
   exit 1
 fi
 
-echo "LS COLOR FLAG: $LSColorFlag"
-
 # make back-up of origional bashrc
+[ -f $HOME/.bashrc ] && echo "bashrc exists"
+[ -f $HOME/.bash_profile ] && echo "bash_profile exists"
+[ -f $HOME/.profile ] && echo "profile exits"
+echo ''
+
+echo "LS COLOR FLAG: $LSColorFlag"
 # set convienence aliases
 # note: intentional use of double qoutes and \"
 #       single quotes dont expand variables, and
