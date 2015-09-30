4c3579cf1b65db7e16f61fa747ba02a585e9f1d0
Duncan Marsh <dwolfm@gmail.com>
2015-09-30 15:35:37 -0700

    fixed xor conditionals, added linking, and made pretty script banner

--- src/bash-starter.sh
+++ src/bash-starter.sh
@@ -2,38 +2,46 @@
 # check os
 # backup old bashconf
 # install bash git aware prompt
-# set PS1
-# set convienece alisus
+# curl bashrc
 # link bashrc bash_profile and profile to be the same
 
+# ansi color characters
 PNK="\e[95m"
 RED="\e[91m"
 CYN="\e[96m" 
 WHT="\e[39m"
 
+echo -e "${PNK}###  ${RED} #   ${CYN} ## ${WHT}#  #   ${PNK}###  ${RED}#  # ${CYN}#    ${WHT}#### ${PNK} ## ${RED} #" 
+echo -e "${PNK}#  # ${RED}# #  ${CYN}#   ${WHT}#  #   ${PNK}#  # ${RED}#  # ${CYN}#    ${WHT}#    ${PNK}#   ${RED} #"
+echo -e "${PNK}#  # ${RED}#  # ${CYN}#   ${WHT}#  #   ${PNK}#  # ${RED}#  # ${CYN}#    ${WHT}#    ${PNK}#   ${RED} #"
+echo -e "${PNK}###  ${RED}#### ${CYN} #  ${WHT}####   ${PNK}###  ${RED}#  # ${CYN}#    ${WHT}#### ${PNK} #  ${RED} #"
+echo -e "${PNK}#  # ${RED}#  # ${CYN}  # ${WHT}#  #   ${PNK}#  # ${RED}#  # ${CYN}#    ${WHT}#    ${PNK}  # ${RED} #"
+echo -e "${PNK}#  # ${RED}#  # ${CYN}  # ${WHT}#  #   ${PNK}#  # ${RED}#  # ${CYN}#    ${WHT}#    ${PNK}  # ${RED}  "
+echo -e "${PNK}###  ${RED}#  # ${CYN}##  ${WHT}#  #   ${PNK}#  # ${RED} ##  ${CYN}#### ${WHT}#### ${PNK}##  ${RED} #"
+
 debug () {
-  if [ $DEBUG = "ENABLED" ]; then
-    echo -e ${PNK}${1}${WHT}
+  if [ $debug = True ]; then
+    echo -e ${PNK}${@}${WHT}
   fi
 }
 
-USAGE_ERROR="true"
-[ $# -eq 0 ] || [ $# -eq 2 ] && USAGE_ERROR="flase"
-if [ $USAGE_ERROR = "true" ]; then 
+# check for usage error
+if ! $([ $# -eq 0 ] || [ $# -eq 2 ]); then 
   echo -e "$RED USAGE ERROR:"
-  echo -e "    $CYN Run Installer:$WHT ./bash-starter.sh"
-  echo -e "    $CYN Development:$WHT ./bash-starter.sh --test <Linux || Darwin>" 
+  echo -e "    ${CYN}Run Installer:${WHT} ./bash-starter.sh"
+  echo -e "    ${CYN}Development:${WHT} ./bash-starter.sh --test <Linux || Darwin>" 
   exit 1
 fi
 
+# setup script variables
 if [ $# -eq 2 ]; then
-  DEBUG="ENABLED"
+  debug=True
   OS="$2"
   HOME="./USER_TEST"
   [ ! -d $HOME ] && mkdir $HOME
-  echo "" > $HOME/.profile
-  echo "" > $HOME/.bashrc
-  echo "" > $HOME/.bash_profile
+  #echo "" > $HOME/.profile
+  #echo "" > $HOME/.bashrc
+  #echo "" > $HOME/.bash_profile
 else 
   echo -e $RED "exiting here for test so i dont break my computer"
   exit 0 
@@ -42,9 +50,10 @@ fi
 debug "OS: $OS\n"
 
 # only run script for linux or mac
-OS_ERROR="true"
-[ $OS = "Linux" ] || [ $OS = "Darwin" ] && OS_ERROR="false"
-[ $OS_ERROR = "true" ] && echo -e "${RED}ERROR:${WHT} This script must be run on a Linux or OS X Operating System." && exit 1
+if ! $([ $OS = "Linux" ] || [ $OS = "Darwin" ]); then 
+  echo -e "${RED}ERROR:${WHT} This script must be run on a Linux or OS X Operating System."
+  exit 1
+fi
 
 # make back-up of origional bashrc
 if [ -f $HOME/.bashrc ]; then 
@@ -64,7 +73,7 @@ fi
 [ ! -d $HOME/.bash ] && mkdir $HOME/.bash
 if [ ! -d $HOME/.bash/git-aware-prompt ]; then
   # download git-aware prompt
-  echo -e $CYN"Installing git-aware-promt:$WHT"
+  echo -e "${CYN}Installing git-aware-promt:${WHT}"
   mkdir $HOME/.bash/git-aware-prompt
   git clone git://github.com/jimeh/git-aware-prompt.git $HOME/.bash/git-aware-prompt
   echo ""
@@ -72,8 +81,12 @@ fi
 
 # create $HOME/.bashrc
 BASH_PATH=$HOME/.bashrc
-curl -p $BASH_PATH 
+echo -e "${CYN}Installing bashrc:${WHT}"
+curl -o $BASH_PATH https://raw.githubusercontent.com/slugbyte/code-101-prework/master/src/bashrc
 
 # make links so bashrc bash_profile and profile are all the same
+echo -e "${CYN}Linking ~/.profile and ~/.bash_profile to ~/.bashrc${WHT}"
+ln -sf $HOME/.bashrc $HOME/.bash_profile
+ln -sf $HOME/.bashrc $HOME/.profile
 
 echo -e "$WARNING"
