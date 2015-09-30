2be6d4e07fbbe729ac5d253117d2dca115f3a92f
Duncan Marsh <dwolfm@gmail.com>
2015-09-30 11:49:12 -0700

    added swp files to gitignore

--- src/bash-starter.sh
+++ src/bash-starter.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 # check os
 # backup old bashconf
 # install bash git aware prompt
@@ -7,14 +6,41 @@
 # set convienece alisus
 # link bashrc bash_profile and profile to be the same
 
-echo "OS input $1"
-echo ""
+debug () {
+  if [ $DEBUG = "ENABLED" ]; then
+    echo -e $1
+  fi
+}
 
-HOME="./USER_TEST"
+RED="\e[91m"
+CYN="\e[96m" 
+WHT="\e[39m"
 
-# check os
-OS=$1
+USAGE_ERROR="true"
+[ $# -eq 0 ] || [ $# -eq 2 ] && USAGE_ERROR="flase"
+if [ $USAGE_ERROR = "true" ]; then 
+  echo -e "$RED USAGE ERROR:"
+  echo -e "    $CYN Run Installer:$WHT ./bash-starter.sh"
+  echo -e "    $CYN Development:$WHT ./bash-starter.sh --test <Linux || Darwin>" 
+  exit 1
+fi
 
+if [ $# -eq 2 ]; then
+  DEBUG="ENABLED"
+  OS="$2"
+  HOME="./USER_TEST"
+  [ ! -d $HOME ] && mkdir $HOME
+  echo "" > $HOME/.profile
+  echo "" > $HOME/.bashrc
+  echo "" > $HOME/.bash_profile
+else 
+  echo -e $RED "exiting here for test so i dont break my computer"
+  exit 0 
+  OS=`uname`
+fi
+debug "OS: $OS\n"
+
+# check os and set color flag
 if [ $OS = "Linux" ]; then 
   LSColorFlag="--color=auto"
 elif [ $OS = "Darwin" ]; then
@@ -23,25 +49,50 @@ else
   echo "This script must be run on a Linux or OS X Operating System."
   exit 1
 fi
+debug "LS COLOR FLAG: $LSColorFlag\n"
 
 # make back-up of origional bashrc
-[ -f $HOME/.bashrc ] && echo "bashrc exists"
-[ -f $HOME/.bash_profile ] && echo "bash_profile exists"
-[ -f $HOME/.profile ] && echo "profile exits"
-echo ''
+if [ -f $HOME/.bashrc ]; then 
+  WARNING="$WARNING\n${RED}Warning:${WHT} your original${CYN} $HOME/.bashrc${WHT} has been moved to${CYN} $HOME/.bashrc.back"
+  mv $HOME/.bashrc $HOME/.bashrc.back 
+fi 
+if [ -f $HOME/.bash_profile ]; then 
+  mv $HOME/.bash_profile $HOME/.bash_profile.back
+  WARNING="$WARNING\n${RED}Warning:${WHT} your original${CYN} $HOME/.bash_profile${WHT} has been moved to${CYN} $HOME/.bash_profile.back"
+fi
+if [ -f $HOME/.profile ]; then 
+  mv $HOME/.profile $HOME/.profile.back
+  WARNING="$WARNING\n${RED}Warning:${WHT} your original${CYN} $HOME/.profile${WHT} has been moved to${CYN} $HOME/.profile.back"
+fi
 
-echo "LS COLOR FLAG: $LSColorFlag"
+# make a $HOME/.bash dir if doesnt all read exist
+[ ! -d $HOME/.bash ] && mkdir $HOME/.bash
+if [ ! -d $HOME/.bash/git-aware-prompt ]; then
+  # download git-aware prompt
+  echo -e $CYN"Installing git-aware-promt:$WHT"
+  mkdir $HOME/.bash/git-aware-prompt
+  git clone git://github.com/jimeh/git-aware-prompt.git $HOME/.bash/git-aware-prompt
+  echo ""
+fi
+
+# create $HOME/.bashrc
+BASH_PATH=$HOME/.bashrc
+
+echo "export GITAWAREPROMPT=\$HOME/.bash/git-aware-prompt" >> $BASH_PATH
+echo "source \"\${GITAWAREPROMPT}/main.sh\""               >> $BASH_PATH
+echo ""                                                   >> $BASH_PATH
 # set convienence aliases
 # note: intentional use of double qoutes and \"
 #       single quotes dont expand variables, and
 #       I want their files to have double qoutes.
-echo "alias ls=\"ls $LSColorFlag\""
-echo "alias la=\"ls -a\""
-echo "alias ll=\"ls -lh\""
-echo "alias lal=\"ls -lah\""
-echo "alias ..=\"cd ..\""
+echo "alias ls=\"ls $LSColorFlag\"" >> $BASH_PATH
+echo "alias la=\"ls -a\""           >> $BASH_PATH
+echo "alias ll=\"ls -lh\""          >> $BASH_PATH
+echo "alias lal=\"ls -lah\""        >> $BASH_PATH
+echo ""                             >> $BASH_PATH
+echo "alias ..=\"cd ..\""           >> $BASH_PATH
 
-# download bash-git help
 # set new ps1
 # make links so bashrc bash_profile and profile are all the same
 
+echo -e "$WARNING"
